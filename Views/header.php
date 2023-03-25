4bdb37723a0f08ebeb7d7d9d9ba8618dc723046c
Edgar Apaza <edgarapazac@gmail.com>
2023-03-25 15:10:39 -0500

    Limpiando todo

--- Views/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Solicitudes</title>
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-</head>
-<body>
